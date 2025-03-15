0a571e09ac2dc041c86d672424babad9b5838476
yasin abbasspour <yasin.absr83@gmail.com>
2025-03-15 22:35:51 +0330

    update

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,13 @@ mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URL),
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      };
-    console.log("<------------ MongoDB database Connected ------------>");
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    };
+    console.log("<------------ MongoDB database Connected ------------>", port);
   } catch (err) {
     console.log(
       "<------------ MongoDB database Connection Faild ------------>",
